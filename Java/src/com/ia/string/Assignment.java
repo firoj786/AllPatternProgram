33e09154b55ced30342239fdf70bbb8cfc348f20
FIROJ <FIROJ@192.168.0.8>
2019-04-14 19:46:47 +0530

    Pattern Program in java 15-05-2019

---
+++ Java/src/com/ia/string/Assignment.java
@@ -0,0 +1,22 @@
+package com.ia.string;
+
+
+public class Assignment{
+	public static void main(String[] args) {
+		
+		    {
+		       // signed //CE
+		    	int x = 10;
+		        for (int y=0; y<5; y++, x--)
+		            System.out.print(x + ", ");
+		    }
+		}
+	    
+	}
+
+
+		
+		
+	
+
+
---
+++ Java/src/com/ia/string/Logical.java
@@ -0,0 +1,9 @@
+package com.ia.string;
+public class Logical {
+public static void main(String[] args) {
+		int a=4,b=8,c=3,d=9,z;
+	    z=a++ + ++b * c-- - --d;
+	    System.out.println("z:"+z);
+	}
+
+}
